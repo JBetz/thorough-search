bdf165c579cc678d9d8931c0e920d4f450db2d49
JBetz <joebetz91@gmail.com>
2017-11-26 15:21:00 -0600

    added retry mechanism to recursive autocomplete

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lib
-    ( completeInstasearch
-    , instasearch
+    ( autocomplete
+    , recursiveAutocomplete
     ) where
 
 import Network.Wreq
 import Control.Lens
 import Control.Concurrent
+import Control.Exception
 import Data.Aeson (eitherDecode)
 import Data.ByteString.Lazy (ByteString)
 import Data.List (isSuffixOf, sort, union)
@@ -16,27 +17,36 @@ import Data.Text (pack)
 
 alphabet :: String
 alphabet =
-  "abcdefghijklmnopqrstuvwzyz"
+  "abcdefghijklmnopqrstuvwxyz"
 
-completeInstasearch :: String -> Int -> IO [String]
-completeInstasearch query maxWordCount = do
-  (_, initialResults) <- instasearch query
-  results <- runCompleteInstasearch (query ++ " ")
-  pure $ sort (filter (\r -> length (words r) <= maxWordCount) (initialResults `union` results))
+recursiveAutocomplete :: String -> IO [String]
+recursiveAutocomplete query = do
+  (_, initialResults) <- autocomplete query
+  results <- runRecursiveAutocomplete (query ++ " ")
+  pure $ sort (initialResults `union` results)
 
-runCompleteInstasearch :: String -> IO [String]
-runCompleteInstasearch query = do
+runRecursiveAutocomplete :: String -> IO [String]
+runRecursiveAutocomplete query = do
   print query
-  results <- sequence $ fmap instasearch (expandQuery query)
-  recursiveResults <- sequence $ fmap runCompleteInstasearch (findExpandables results)
+  results <- sequence $ fmap autocompleteWithRetry (expandQuery query)
+  recursiveResults <- sequence $ fmap runRecursiveAutocomplete (findExpandables results)
   pure $ concatMap snd results `union` concat recursiveResults
 
-instasearch :: String -> IO (String, [String])
-instasearch search = do
-  threadDelay 500000
-  let opts = defaults & param "q" .~ [pack search] & param "client" .~ ["firefox"]
+autocompleteWithRetry :: String -> IO (String, [String])
+autocompleteWithRetry query =
+  catch (autocomplete query)
+        (\e -> do
+          print (e :: IOException)
+          msThreadDelay 30000
+          autocomplete query
+        )
+
+autocomplete :: String -> IO (String, [String])
+autocomplete query = do
+  msThreadDelay 25
+  let opts = defaults & param "q" .~ [pack query] & param "client" .~ ["firefox"]
   response <- getWith opts "https://www.google.com/complete/search"
-  pure $ parseResponse (response ^. responseBody) search
+  pure $ parseResponse (response ^. responseBody) query
 
 expandQuery :: String -> [String]
 expandQuery baseQuery =
@@ -52,3 +62,7 @@ parseResponse response def =
   case eitherDecode response :: Either String (String, [String]) of
     Left err -> (def, [])
     Right (key, vals) -> (key, vals)
+
+msThreadDelay :: Int -> IO ()
+msThreadDelay ms =
+  threadDelay $ ms * 1000
