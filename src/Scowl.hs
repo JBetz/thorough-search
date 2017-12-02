ed143cc6f1b8639523434defac6dbec3398d4f4d
JBetz <joebetz91@gmail.com>
2017-12-02 00:21:19 -0600

    interface to SCOWL word lists

---
+++ src/Scowl.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Scowl
+  ( hasWord
+  , loadWordsFromScowl
+  , Size(..)
+  ) where
+
+import           Control.Monad
+import           System.IO
+
+data Size
+  = S10
+  | S20
+  | S35
+  | S40
+  | S50
+  | S55
+  | S60
+  | S70
+  | S80
+  | S95
+  deriving (Read, Enum)
+
+show :: Size -> String
+show size =
+  Prelude.show $
+  case size of
+    S10 -> 10
+    S20 -> 20
+    S35 -> 35
+    S40 -> 40
+    S50 -> 50
+    S55 -> 55
+    S60 -> 60
+    S70 -> 70
+    S80 -> 80
+    S95 -> 95
+
+hasWord :: String -> IO [String] -> IO Bool
+hasWord word ioList = do
+  wordList <- ioList
+  pure $ elem word wordList
+
+loadWordsFromScowl :: Size -> IO [String]
+loadWordsFromScowl size = do
+  words <-
+    traverse
+      (\s -> loadWordsFromFile ("./scowl/final/english-words." ++ Scowl.show s))
+      (enumFromTo S10 size)
+  pure $ join words
+
+loadWordsFromFile :: FilePath -> IO [String]
+loadWordsFromFile path = do
+  fileContents <- readFile path
+  pure $ lines fileContents
