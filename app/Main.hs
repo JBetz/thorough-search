74c944080aae41c616c4ffc4a96ab81c1b732aa6
JBetz <joebetz91@gmail.com>
2017-12-02 03:38:13 -0600

    integrating scowl word list for filtering results

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ module Main where
 import           Config
 import           Control.Monad.Logger    (runNoLoggingT)
 import           Control.Monad.Reader
-import           Data.List               (sort)
+import           Data.List               (sort, (\\), isPrefixOf)
 import           Data.Text               (Text)
 import           Database.Persist.Sqlite (createSqlitePool, runMigration,
                                           runSqlPool)
@@ -21,10 +21,27 @@ main = do
   runNoLoggingT $ runSqlPool (runMigration migrateAll) connectionPool
   let baseQuery = head args
   let scowlSize = read $ args !! 1
+  let maxWordCount = read $ args !! 2
   wordList <- loadWordsFromScowl scowlSize
   let config = Config baseQuery wordList connectionPool
   result <- runReaderT (recursiveInstasearch $ baseQuery ++ " ") config
   print $ show (length result) ++ " results recorded"
+  totalResults <- runReaderT selectResults config
+  print $ show (length totalResults) ++ " total results"
+  let extractedResults = fmap resultValue totalResults
+  let filteredResults =
+        filter
+          (\r ->
+             let rWords = words r
+             in length rWords <= maxWordCount &&
+                (baseQuery `isPrefixOf` head rWords) && null (rWords \\ wordList))
+          extractedResults
+  print $ show (length filteredResults) ++ " filtered results"
+  output <-
+    traverse
+      (\r -> appendFile (baseQuery ++ ".txt") (r ++ "\n"))
+      (sort filteredResults)
+  pure ()
 
 connStr :: Text
 connStr = "file:./output/autocomplete.db"
