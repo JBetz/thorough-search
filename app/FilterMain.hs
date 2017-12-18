cf2e6d64c2b1e75af19048b060f0a7d558d244e9
JBetz <joebetz91@gmail.com>
2017-12-18 12:00:35 -0600

    passing expanded query value to scowl filter

--- app/FilterMain.hs
+++ app/FilterMain.hs
@@ -4,7 +4,7 @@ module FilterMain where
 
 import           Config
 import           Control.Monad.Reader
-import           Data.Set               (fromList)
+import           Data.List              (nubBy)
 import           Data.Text              (unpack)
 import           Database.SQLite.Simple (close, open)
 import           Scowl
@@ -14,15 +14,16 @@ import           System.Environment     (getArgs)
 main :: IO ()
 main = do
   args <- getArgs
-  connection <- open connStr
+  conn <- open connStr
   let query = head args
-  let scowlSize = read $ args !! 1
-  let config = Config query connection
-  wordLists <- loadWordsFromScowl (fromInt scowlSize)
+  let scowlSize = fromInt $ read (args !! 1)
+  let config = Config query conn
+  wordLists <- loadWordsFromScowl scowlSize
   dbResults <- runReaderT selectResults config
-  close connection
+  close conn
   print $ show (length dbResults) ++ " total results"
-  let results = fromList $ fmap (\(ResultsField _ _ _ v) -> unpack v) dbResults
+  let results = nubBy cmpKey $ fmap (\(ResultsField _ _ eq v) -> (,) (unpack eq) (unpack v)) dbResults
+        where cmpKey (_, y) (_, y') = y == y'
   let filteredResults = fmap (filterResults query results) wordLists
-  output <- traverse (writeWordsToFile query) filteredResults
+  output <- traverse (uncurry (writeWordsToFile query)) (zip (enumFromTo S10 scowlSize) filteredResults)
   print $ show (length output) ++ " filtered results"
