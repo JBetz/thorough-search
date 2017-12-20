1765aae10c11032fcccfe45f00f8dae52a6883d3
JBetz <joebetz91@gmail.com>
2017-12-20 12:38:46 -0600

    increasing initial max query length to 3

--- app/SearchMain.hs
+++ app/SearchMain.hs
@@ -18,7 +18,7 @@ main = do
   let actions = do
         createQueriesTable
         createResultsTable
-        recursiveInstasearch (query ++ " ") 1
+        recursiveInstasearch (query ++ " ") 3
   result <- runReaderT actions config
   close conn
   print $ show (length result) ++ " results recorded"
