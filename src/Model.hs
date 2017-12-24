5d434360588a0358e9a894aba74653c392e544f6
JBetz <joebetz91@gmail.com>
2017-12-24 07:06:11 -0600

    created difference function for extracting relevant words from query result

--- src/Model.hs
+++ src/Model.hs
@@ -2,12 +2,14 @@ module Model
   ( Query(..)
   , Structure(..)
   , matches
+  , difference
   , readStr
   , show_
   ) where
 
 import Data.List
 
+-- DATATYPES
 data Query = Query
   { base :: String 
   , expansion :: String
@@ -17,6 +19,7 @@ data Query = Query
 data Structure = WordX | WordOfX | XWord
   deriving (Show)
 
+-- PRINTING
 instance Show Query where
   show (Query b e s) =
     case s of
@@ -24,6 +27,27 @@ instance Show Query where
       WordOfX -> b ++ " of " ++ e
       XWord -> e ++ " " ++ b
 
+show_ :: Query -> String
+show_ q@(Query _ _ s) = 
+  let sanitized = fmap (\c -> if c == ' ' then '_' else c) (show q) 
+    in case s of 
+      XWord -> 'X' : sanitized
+      _ ->  sanitized ++ "X"
+
+-- PARSING
+readStr :: String -> Query
+readStr string = 
+  let mIndex = elemIndex 'X' string
+      strLength = length string
+  in case mIndex of
+      Just 0 -> Query (drop 2 string) "" XWord 
+      Just index -> 
+        if index == strLength - 1 
+          then Query (take (strLength - 2) string) "" WordX
+          else Query (take (strLength - 5) string) "" WordOfX
+      Nothing -> error "invalid query, needs to be of form '<word> X', '<word> of X', or 'X <word>'"
+
+-- TESTING
 matches :: Query -> String -> Bool
 matches (Query b _ s) result = 
   case s of 
@@ -31,21 +55,10 @@ matches (Query b _ s) result =
     WordOfX -> take 2 (words result) == words b
     XWord -> last (words result) == b
 
-readStr :: String -> Query
-readStr string = 
-  let mIndex = elemIndex 'X' string
-      strLength = length string
-  in case mIndex of
-    Just 0 -> Query (drop 2 string) "" XWord 
-    Just index -> 
-        if index == strLength - 1 
-            then Query (take (strLength - 2) string) "" WordX
-            else Query (take (strLength - 5) string) "" WordOfX
-    Nothing -> error "invalid query, needs to be of form '<word> X', '<word> of X', or 'X <word>'"
-
-show_ :: Query -> String
-show_ q@(Query _ _ s) = 
-  let sanitized = fmap (\c -> if c == ' ' then '_' else c) (show q) 
+difference :: Query -> String -> [String]
+difference (Query _ _ s) result = 
+  let rWords = words result
   in case s of 
-       XWord -> 'X' : sanitized
-       _ ->  sanitized ++ "X"
+      WordX -> drop 1 rWords
+      WordOfX -> drop 2 rWords
+      XWord -> take (length rWords - 2) rWords
