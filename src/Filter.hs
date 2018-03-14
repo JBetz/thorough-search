f7cdcb77c104c5602b18e3b143765e15af3bd99e
JBetz <joebetz91@gmail.com>
2018-03-14 16:33:24 -0500

    corrected sort function

--- src/Filter.hs
+++ src/Filter.hs
@@ -12,7 +12,8 @@ module Filter
 import Prelude hiding (filter)
 import Config
 import Control.Monad
-import Data.List ((\\), groupBy, union)
+import Data.List ((\\), groupBy, sortBy)
+import qualified Data.Set as S
 import Model
 
 -- FILTERS
@@ -23,8 +24,14 @@ data Result = Result
 
 data FilteredResult = FilteredResult Result Size
 
+queryLength :: FilteredResult -> Int
+queryLength (FilteredResult r _) = length (_query r)
+
 instance Eq Result where
   (==) a b = _result a == _result b
+
+instance Ord Result where 
+  (<=) a b = _result a <= _result b
 
 instance Eq FilteredResult where 
   (==) (FilteredResult r1 _) (FilteredResult r2 _) =
@@ -46,31 +53,28 @@ byScowlSet q unfiltered filtered wordLists =
     then filtered
   else 
     let (x:xs) = wordLists
-        newFiltered = runFilter q unfiltered filtered x
+        newFiltered = runFilter q unfiltered x
         allFiltered = filtered ++ newFiltered
     in byScowlSet q (unfiltered \\ ((\(FilteredResult r _) -> r) <$> newFiltered)) allFiltered xs 
 
-runFilter :: Query -> [Result] -> [FilteredResult] -> WordList -> [FilteredResult]
-runFilter (Query _ _ s) unfiltered filtered wordList = do
+runFilter :: Query -> [Result] -> WordList -> [FilteredResult]
+runFilter (Query _ _ s) unfiltered wordList = do
   result <- unfiltered
-  let resultDiff = extractExpansion s (_result result)
-  guard $
-    null (resultDiff \\ _words wordList) &&
-    (_result result `notElem` (_result <$> (\(FilteredResult r _) -> r) <$> filtered))
+  let resultDiff = S.fromList $ extractExpansion s (_result result)
+  guard $ null (resultDiff S.\\ _words wordList)
   pure $ FilteredResult result (_size wordList)
 
 -- SORTERS
 sort :: [FilteredResult] -> [[FilteredResult]]
-sort results = 
-  groupBy (\(FilteredResult r1 _) (FilteredResult r2 _) -> 
-    (length . _query $ r1) == (length . _query $ r2)
-  ) results
+sort results =
+  let sortedResults = sortBy (\r1 r2 -> compare (queryLength r1) (queryLength r2)) results
+  in groupBy (\r1 r2 -> queryLength r1 == queryLength r2) sortedResults
    
 
 -- SCOWL WORD LISTS
 data WordList = WordList
  { _size :: Size 
- , _words :: [String]
+ , _words :: S.Set String
  } 
 
 data Size
@@ -99,7 +103,7 @@ loadWordList :: Size -> [String] -> IO WordList
 loadWordList size names = do
   let fileNames = fmap (\n -> "./scowl/" ++ n ++ "." ++ show (toInt size)) names
   fileContents <- traverse readFile fileNames
-  pure $ WordList size (join (fmap lines fileContents))
+  pure $ WordList size (S.fromList $ join (fmap lines fileContents))
 
 accumulatedWordLists :: [WordList] -> [WordList]
 accumulatedWordLists wordLists =
@@ -107,4 +111,4 @@ accumulatedWordLists wordLists =
 
 combine :: [WordList] -> WordList
 combine wordLists = 
-  WordList (_size $ last wordLists) (foldl (\a b -> a `union` _words b) [] wordLists) 
+  WordList (_size $ last wordLists) (foldl (\a b -> a `S.union` _words b) S.empty wordLists) 
