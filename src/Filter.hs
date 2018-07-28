7c17d6eb30c777521c46849c3e341cb669303f5d
Joe Betz <joebetz91@gmail.com>
2018-07-28 23:30:51 +0200

    sorting by scowl list rather than query length

--- src/Filter.hs
+++ src/Filter.hs
@@ -9,37 +9,37 @@ module Filter
   , FilteredResult(..)
   ) where
 
-import Prelude hiding (filter)
-import Config
-import Control.Monad
-import Data.List ((\\), groupBy, sortBy)
-import qualified Data.Set as S
-import Model
+import           Config
+import           Control.Monad
+import           Data.List     (groupBy, sortBy, (\\))
+import qualified Data.Set      as S
+import           Model
+import           Prelude       hiding (filter)
 
 -- FILTERS
 data Result = Result
-  { _query :: String
-  , _result :: String
+  { query  :: String
+  , result :: String
   }
 
 data FilteredResult = FilteredResult Result Size
 
 queryLength :: FilteredResult -> Int
-queryLength (FilteredResult r _) = length (_query r)
+queryLength (FilteredResult r _) = length (query r)
 
 instance Eq Result where
-  (==) a b = _result a == _result b
+  (==) a b = result a == result b
 
-instance Ord Result where 
-  (<=) a b = _result a <= _result b
+instance Ord Result where
+  (<=) a b = result a <= result b
 
-instance Eq FilteredResult where 
+instance Eq FilteredResult where
   (==) (FilteredResult r1 _) (FilteredResult r2 _) =
     r1 == r2
 
-instance Ord FilteredResult where 
+instance Ord FilteredResult where
   (<=) (FilteredResult r1 _) (FilteredResult r2 _) =
-    _result r1 <= _result r2 
+    result r1 <= result r2
 
 filter :: Query -> [Result] -> FilterConfig -> IO [FilteredResult]
 filter q results (FilterConfig sws) = do
@@ -48,34 +48,36 @@ filter q results (FilterConfig sws) = do
   pure $ byScowlSet q results [] accWordLists
 
 byScowlSet :: Query -> [Result] -> [FilteredResult] -> [WordList] -> [FilteredResult]
+byScowlSet _ _ filtered [] = filtered
 byScowlSet q unfiltered filtered wordLists =
-  if null wordLists
-    then filtered
-  else 
-    let (x:xs) = wordLists
-        newFiltered = runFilter q unfiltered x
-        allFiltered = filtered ++ newFiltered
-    in byScowlSet q (unfiltered \\ ((\(FilteredResult r _) -> r) <$> newFiltered)) allFiltered xs 
+  let (x:xs) = wordLists
+      newFiltered = runFilter q unfiltered x
+      allFiltered = filtered ++ newFiltered
+  in byScowlSet q (unfiltered \\ ((\(FilteredResult r _) -> r) <$> newFiltered)) allFiltered xs
 
 runFilter :: Query -> [Result] -> WordList -> [FilteredResult]
 runFilter (Query _ _ s) unfiltered wordList = do
-  result <- unfiltered
-  let resultDiff = S.fromList $ extractExpansion s (_result result)
+  res <- unfiltered
+  let resultDiff = S.fromList $ extractExpansion s (result res)
   guard $ null (resultDiff S.\\ _words wordList)
-  pure $ FilteredResult result (_size wordList)
+  pure $ FilteredResult res (_size wordList)
 
 -- SORTERS
 sort :: [FilteredResult] -> [[FilteredResult]]
 sort results =
+  let sortedResults = sortBy (\(FilteredResult _ size1) (FilteredResult _ size2) -> compare size1 size2) results
+  in groupBy (\(FilteredResult _ size1) (FilteredResult _ size2) -> size1 == size2) sortedResults
+
+sortByQueryLength :: [FilteredResult] -> [[FilteredResult]]
+sortByQueryLength results =
   let sortedResults = sortBy (\r1 r2 -> compare (queryLength r1) (queryLength r2)) results
   in groupBy (\r1 r2 -> queryLength r1 == queryLength r2) sortedResults
-   
 
 -- SCOWL WORD LISTS
 data WordList = WordList
- { _size :: Size 
+ { _size  :: Size
  , _words :: S.Set String
- } 
+ }
 
 data Size
   = S10
@@ -88,7 +90,7 @@ data Size
   | S70
   | S80
   | S95
-  deriving (Read, Enum, Show)
+  deriving (Eq, Ord, Read, Enum, Show)
 
 toInt :: Size -> Int
 toInt size = read $ drop 1 (show size)
@@ -110,5 +112,5 @@ accumulatedWordLists wordLists =
   fmap (\i -> combine $ take i wordLists) [1 .. length wordLists]
 
 combine :: [WordList] -> WordList
-combine wordLists = 
-  WordList (_size $ last wordLists) (foldl (\a b -> a `S.union` _words b) S.empty wordLists) 
+combine wordLists =
+  WordList (_size $ last wordLists) (foldl (\a b -> a `S.union` _words b) S.empty wordLists)
